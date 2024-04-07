ba54a7069afff06d0595b6f45ed1d9b6f7252bde
Samantha Cruz <cruz.s98319@gmail.com>
2024-04-07 11:50:45 -0700

    Tested out babel with different equation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const multiply = (a,b) => a * b
+const add = (a,b) => a + b
 const firstNum = 2
 const secondNum = 3
-let product = multiply(firstNum, secondNum)
+let product = add(firstNum, secondNum)
 
 console.log(`The product of ${firstNum} and ${secondNum} is ${product}`)
